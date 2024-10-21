8e6d66c20ba1e21639c2a5a42f40dda04482ff1d
Sarahwcode <sarahw181090@gmail.com>
2024-10-21 16:25:01 +0100

    commit user expereince

--- Source/index.js
+++ Source/index.js
@@ -1,5 +1,4 @@
 function poemDisplay(response) {
-  console.log("poem generated");
   new Typewriter(".poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -12,13 +11,12 @@ function changePoem(event) {
   event.preventDefault();
   let instructionsForPoem = document.querySelector("#instructions-poem");
   let apiKey = "145ba0dc50b14bt75oafffd6a19b4a36";
-  let prompt = `user instructions are to Generate a poem from ${instructionsForPoem}`;
+  let prompt = `user instructions are to Generate a poem from ${instructionsForPoem.value}`;
   let context =
     "you are thinking of taylor swift poems and try and relate the context to taylor swift poem/music, keep this small and simple and only small lines. sign at the end of the poem the artist who wrote it and make the word <strong> and each line is seperated by <p>.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-  console.log("generating poem");
-  console.log(`promt: ${prompt}`);
-  console.log(`context: ${context}`);
+  let poemElement = document.querySelector(".generating");
+  poemElement.innerHTML = `<div class="blink">Generating Poem about ${instructionsForPoem.value}....</div>`;
 
   axios.get(apiUrl).then(poemDisplay);
 }
