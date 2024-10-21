003ff7533a14efa840546b32570e2d2dc274cdfb
Sarahwcode <sarahw181090@gmail.com>
2024-10-21 09:58:07 +0100

    added java script

--- Source/index.js
+++ Source/index.js
@@ -1,1 +1,13 @@
+function changePoem(event) {
+  event.preventDefault();
 
+  new Typewriter(".poem", {
+    strings: "The Trick to Holding On",
+    autoStart: true,
+    cursor: null,
+    delay: 50,
+  });
+}
+
+let formSubmit = document.querySelector("#poem-form");
+formSubmit.addEventListener("submit", changePoem);
