1d1ad3e66b5f133b404cc30dc54c20b88b7349af
Sarahwcode <sarahw181090@gmail.com>
2024-10-21 11:21:22 +0100

    commit ai integration

--- Source/index.js
+++ Source/index.js
@@ -1,12 +1,26 @@
-function changePoem(event) {
-  event.preventDefault();
-
+function poemDisplay(response) {
+  console.log("poem generated");
   new Typewriter(".poem", {
-    strings: "The Trick to Holding On",
+    strings: response.data.answer,
     autoStart: true,
     cursor: null,
     delay: 50,
   });
+}
+
+function changePoem(event) {
+  event.preventDefault();
+  let instructionsForPoem = document.querySelector("#instructions-poem");
+  let apiKey = "145ba0dc50b14bt75oafffd6a19b4a36";
+  let prompt = `user instructions are to Generate a poem from ${instructionsForPoem}`;
+  let context =
+    "you are thinking of taylor swift poems and try and relate the context to taylor swift poem/music, keep this small and simple and only small lines. sign at the end of the poem the artist who wrote it and make the word <strong> and each line is seperated by <p>.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  console.log("generating poem");
+  console.log(`promt: ${prompt}`);
+  console.log(`context: ${context}`);
+
+  axios.get(apiUrl).then(poemDisplay);
 }
 
 let formSubmit = document.querySelector("#poem-form");
